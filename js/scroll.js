7ad8a49f2e67fa3d08e2c59626a2162193189f3f
Hasan Abu Sharekh <hasan0595488660@gmail.com>
2023-01-21 22:14:03 +0200

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $("#nav-one").css("display", "none");
+      $(".logo .logo1").css("width", "42px");
+      $(".logo .logo1").css("height", "42px");
+      $(".logo .logo2").css("width", "92px");
+      $(".logo .logo2").css("height", "30px");
+      $(".hamburger").css("margin-top", "-1rem");
+      $(".hamburger").css("margin-right", "1rem");
+    } else {
+      $("#nav-one").css("display", "flex");
+      $(".logo .logo1").css("width", "84px");
+      $(".logo .logo1").css("height", "84px");
+      $(".logo .logo2").css("width", "184px");
+      $(".logo .logo2").css("height", "60px");
+      $(".hamburger").css("margin-top", "-2rem");
+    }
+  });
+});
---
+++ js/slider-part.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 500,
+    dots: true,
+    centerMode: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          // centerMode: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
+  });
+});
